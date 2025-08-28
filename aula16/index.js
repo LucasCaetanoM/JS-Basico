ccbb22255717c48add632b318b4cd5c533291af8
LucasCaetanoM <lucascaetano2004@icloud.com>
2025-08-28 12:20:38 -0300

    Arrays introdução

---
+++ aula16/index.js
@@ -0,0 +1,12 @@
+let num1 = 9;
+
+
+//console.log(num1 ** 0.5); raiz quadrada
+//let num2 = Math.floor(num1); = arredonda pra baixo
+//let num2 = Math.ceil(num1); = arredendoda pra cima
+//let num2 = Math.round(num1); = arredonda
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); numero maximo
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); numero minimo
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5); numero aleatorio
+
+
---
+++ aula17/index.js
@@ -0,0 +1,34 @@
+//            0123456789101112
+//const nome = 'Lucas Caetano';
+//console.log(nome[10])
+
+//               0        1         2
+const alunos = ['Lucas', 'Bianca', 'Will'];
+
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+//alunos.shift(); remove o primeiro
+//alunos.pop(); remove o ultimo
+
+//alunos.unshift('Luiza'); melhor
+//alunos.unshift('Fábio');
+
+//alunos.push('Luiza'); melhor
+//alunos.push('Fábio');
+
+/*
+alunos[alunos.length] = 'Luiza';
+alunos[alunos.length] = 'Fábio';
+alunos[alunos.length] = 'Luana';
+*/
+
+/*
+alunos[0] = 'eduardo';
+alunos[3] = 'Luiza';
+
+console.log(alunos);
+console.log(alunos[0]);
+console.log(alunos[1])
+*/
