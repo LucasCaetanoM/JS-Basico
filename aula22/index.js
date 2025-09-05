f4f9ac665f0244be8bac38c4215fd2b7a6b609f5
LucasCaetanoM <lucascaetano2004@icloud.com>
2025-09-05 12:35:45 -0300

    operadores de comparação e lógicos

---
+++ aula22/index.js
@@ -0,0 +1,11 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que 
+<= menor igual a 
+== igualdade (valor) ***
+=== igualdade estrita (valor e tipo)
+!= diferente ( valor)
+!== diferente estrito (valor e tipo)
+*/
---
+++ aula23/index.js
@@ -0,0 +1,9 @@
+/*
+Operadores Lógicos
+&& -> and -> E -> todas expressões precisam ser verdadeiras para retornar TRUE
+|| -> or -> ou
+! -> not -> não
+*/
+
+console.log(true && true);
+console.log(true || false);
